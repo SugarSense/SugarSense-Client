e3bb605ae5945be3e9083702c2847b3f5a8e2e15
Lucas Goncalves <lucasgoncalves@free.fr>
2023-03-28 10:56:51 +0200

    ➕| Adding a navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import "./App.css";
 import "./index.css"
 import SideBar from "./components/sidebar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <SideBar />
-      <h1>Hello</h1>
-      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
     </div>
   );
 }
