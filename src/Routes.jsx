6ff5d4947eeb234ef2fad65bbb1900e88a5035d2
Rui Gaspar <ruigaspar03@hotmail.com>
2023-03-29 12:16:50 +0200

    🟢 - hook to get logged user data

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,22 +5,25 @@ import PrivateRoutes from "./utils/PrivateRoutes";
 import Login from "./views/Login";
 import Register from "./views/Register";
 import Cookies from "universal-cookie";
+import AuthContextProvider from "./hooks/useAuth";
 
 function RoutesProvider() {
   const cookies = new Cookies();
   const loggedIn = cookies.get("auth_token");
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-        <Route element={<PrivateRoutes />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/data-graphics" element={<DataGraphics />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route element={<PrivateRoutes />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/data-graphics" element={<DataGraphics />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 }
 
