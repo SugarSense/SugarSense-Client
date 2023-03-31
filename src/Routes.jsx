479bed064d1b343da7f9a30b0e7cc29b8d9b5fa1
Lucas Goncalves <lucasgoncalves@free.fr>
2023-03-31 15:40:05 +0200

    ➕| Adding route for appointement and dashboard

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,23 +4,30 @@ import Home from "./views/Home";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import Login from "./views/Login";
 import Register from "./views/Register";
-import FollowUp from "./views/FollowUp";
 import Cookies from "universal-cookie";
 import AuthContextProvider from "./hooks/useAuth";
+import FollowUp from "./views/FollowUp";
+import Dashboard from "./views/Dashboard";
+import Appointement from "./views/Appointement";
 
 function RoutesProvider() {
   const cookies = new Cookies();
   const loggedIn = cookies.get("auth_token");
   return (
     <AuthContextProvider>
-      <BrowserRouter>
-        <Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route element={<PrivateRoutes />}>
             <Route path="/data-graphics" element={<DataGraphics />} />
             <Route path="/follow-up" element={<FollowUp />} />
+            <Route path="/appointement" element={<Appointement />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Route>
         </Routes>
       </BrowserRouter>
