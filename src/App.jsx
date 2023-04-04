786973ab3acfd3ce39a68cf7ef0896b7bfe1e190
Rui Gaspar <ruigaspar03@hotmail.com>
2023-04-04 10:11:57 +0200

    🟠 - add interval to refresh token for dexcom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import {useEffect} from "react";
 import "./index.css";
 import RoutesProvider from "./Routes";
+import Cookies from "universal-cookie";
+import axios from "axios";
+import {useAuth} from "./hooks/useAuth";
 
 function App() {
+  const cookies = new Cookies();
+  const token = cookies.get("auth_token");
+
+  setInterval(() => {
+    (async () => {
+      if (token) {
+        await axios.get(`${import.meta.env.VITE_API_PATH}/dexcom/authRefresh`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        console.log("refreshed");
+      }
+    })();
+  }, 71000000);
+
   return <RoutesProvider />;
 }
 
